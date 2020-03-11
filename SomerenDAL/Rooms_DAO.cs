aad5abeef86c4935c84c04f41bccefbe5e8af0cc
ThijsvdMarck <54937890+ThijsvdMarck@users.noreply.github.com>
2020-03-11 16:40:10 +0100

    DAO en Service voor Rooms
    DAO en Service voor Rooms

---
+++ SomerenDAL/Rooms_DAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class Rooms_DAO : Base
+    {
+        public List<Student> Db_Get_All_Students()
+        {
+            string query = "SELECT StudentID, Voornaam, Achternaam, GeboorteDatum FROM [Studenten]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+
+            return ReadStudent(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Student> ReadStudent(DataTable dataTable)
+        {
+            List<Student> students = new List<Student>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Student student = new Student()
+                {
+                    Id = (int)dr["StudentID"],
+                    FirstName = (string)dr["Voornaam"],
+                    LastName = (string)dr["Achternaam"],
+                    BirthDate = (DateTime)dr["GeboorteDatum"]
+
+                };
+
+                students.Add(student);
+            }
+            return students;
+        }
+
+    }
+}
---
+++ SomerenLogic/Room_Service.cs
@@ -0,0 +1,26 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenLogic
+{
+    public class Room_Service
+    {
+        Rooms_DAO room_db = new Rooms_DAO();
+        
+        public List<Room> GetRoom()
+        {
+
+
+            return room_db.Db_Get_All_Rooms(); 
+            
+
+        }
+    }
+    
+}
